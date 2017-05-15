dd41a007cc7bac0c6efc66fe0b811139b23d3959
Konrad Eisele <eiselekd@gmail.com>
2017-05-15 19:51:40 +0000

    all

--- Makefile.pre.mk
+++ Makefile.pre.mk
@@ -4,4 +4,4 @@ docker:
 	sudo docker run --rm -ti -v `pwd`:/opt/nodemcu-firmware marcelstoer/nodemcu-build
 
 toolchain:
-	cd esp-open-sdk; make STANDALONE=y | tee $(CURDIR)/log.txt
+	cd esp-open-sdk; git submodule update --init --recursive; make STANDALONE=y | tee $(CURDIR)/log.txt
