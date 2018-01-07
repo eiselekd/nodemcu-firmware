4d9af6e1aafe14b6dd381a95f34339add6d9579a
Konrad Eisele <Konrad Eisele>
2018-01-07 14:51:36 +0000

    all

--- Makefile.pre.mk
+++ Makefile.pre.mk
@@ -5,3 +5,9 @@ docker:
 
 toolchain:
 	cd esp-open-sdk; git submodule update --init --recursive; make STANDALONE=y | tee $(CURDIR)/log.txt
+
+flash-test:
+	tools/esptool.py --port /dev/ttyUSB0 flash_id
+
+flash: 
+	tools/esptool.py --port /dev/ttyUSB0 write_flash -fm dio 0x00000 bin/nodemcu_integer_master_20180107-1347.bin
