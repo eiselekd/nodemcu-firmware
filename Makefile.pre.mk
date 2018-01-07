922f6a6248382055ad43f1787a7fe4a2b5c92ca2
Konrad Eisele <Konrad Eisele>
2018-01-07 15:53:07 +0000

    all

--- Makefile.pre.mk
+++ Makefile.pre.mk
@@ -7,10 +7,17 @@ toolchain:
 	cd esp-open-sdk; git submodule update --init --recursive; make STANDALONE=y | tee $(CURDIR)/log.txt
 
 flash-test:
-	tools/esptool.py --port /dev/ttyUSB0 flash_id
+	esptool/esptool.py --port /dev/ttyUSB0 flash_id
+
+flash-clear:
+	esptool/esptool.py --port /dev/ttyUSB0 erase_flash
 
 flash-split: 
-	tools/esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m -ff 40m 0x00000 bin/0x00000.bin 0x10000 bin/0x10000.bin
+	esptool/esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m -ff 40m 0x00000 bin/0x00000.bin 0x10000 bin/0x10000.bin
 
 flash-bin: 
-	tools/esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m -ff 40m 0x00000 bin/nodemcu_integer_master_20180107-1529.bin
+	esptool/esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m -ff 40m 0x00000 bin/nodemcu_integer_master_20180107-1529.bin
+
+flash-pcm: 
+	esptool/esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m -ff 40m 0x00000 Makefile.pre.mk.bin/firmware_pcm.bin
+
